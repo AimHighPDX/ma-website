08e94990f633dd603d8263907f0be3ed515b6bf3
Preeta Hopwood <preeta.hopwood@aimhighpdx.org>
2024-08-20 16:14:13 -0700

    Roughs of Staff collection done

---
+++ scripts/aria-disclosures.js
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+function initDisclosures() {
+   let allDisclosures = document.querySelectorAll("button[aria-expanded][aria-controls]");
+   for (let disclosure of allDisclosures) {
+      let disclosureDescription = document.getElementById(disclosure.getAttribute("aria-controls"));
+      disclosure.addEventListener("click", toggleDisclosure.bind(null, disclosure, disclosureDescription))
+      if (disclosure.getAttribute("aria-expanded") === "false") {
+         disclosureDescription.toggleAttribute("hidden", true);
+      }
+   }
+}
+
+/**
+ * Click listener for disclosure.
+ * Changes the value of aria-expanded, then shows or hides content based on the new value.
+ * 
+ * @param {HTMLButtonElement} button 
+ * @param {HTMLElement} description
+ */
+function toggleDisclosure(button, description) {
+   let isShown = button.getAttribute("aria-expanded") === "true";
+   button.setAttribute("aria-expanded", !isShown);
+   description.toggleAttribute("hidden", isShown);
+}
+
+initDisclosures();
