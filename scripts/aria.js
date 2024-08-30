209a562f8d8052d65e75fc3181a413279c454989
Preeta Hopwood <preeta.hopwood@aimhighpdx.org>
2024-08-30 15:34:57 -0700

    Doesn't like multiple scripts for some reason

--- scripts/aria.js
+++ scripts/aria.js
@@ -196,6 +196,40 @@ function selectTab(tab) {
    tab.setAttribute("aria-selected", true);
    document.getElementById(tab.getAttribute("aria-controls")).toggleAttribute('hidden', false);
 }
+
+
+/**
+ * Replaces Squarespace inserted elements.
+ */
+
+
+/**
+ * Replacing curriculum summary elements.
+ */
+function replaceCurriculum() {
+   let wrappers = document.querySelectorAll("[id$=curriculum-wrapper] .summary-v2-block");
+
+   for (let wrapper of wrappers) {
+      let container = document.createElement("ul");
+      let summaries = wrapper.getElementsByClassName("summary-item");
+
+      for (let item of summaries) {
+         let element = document.createElement("li");
+         let link = document.createElement("a");
+
+         link.setAttribute("href", item.getElementsByTagName("a")[0].getAttribute("href"));
+         link.textContent = item.textContent.trim();
+         link.classList.add("curriculum-item");
+
+         element.append(link);
+         element.style.backgroundImage = "url(" + item.getElementsByTagName("img")[0].dataset.src + ")";
+         
+         container.append(element);
+      }
+      container.classList.add("curriculum-list");
+      wrapper.replaceChildren(container);
+   }
+}
  
 
 
@@ -210,5 +244,6 @@ function selectTab(tab) {
       initDisclosures(); 
       initNavDisclosure();
       initTabs();
+      replaceCurriculum();
    }, {once:true});
 }());
